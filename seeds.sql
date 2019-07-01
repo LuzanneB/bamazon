7104bf82a94aefef9e39c607f1f449c0c7099fa9
Luzanne Burt <luzannea@gmail.com>
2019-07-01 10:50:37 -0400

    initial file structure

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("blue sofa", "upholstery",499.99, 99);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("red sofa", "upholstery",599.99, 3);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("green sofa", "upholstery",699.99, 49);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("dresser", "casegoods",499.99, 79);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("queen bed", "casegoods",599.99, 49);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("nightstand", "casegoods",299.99, 90);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("lamp", "lighting",199.99, 50);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("chandelier", "lighting",399.99, 5);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("vase", "accessories",49.99, 6);
+
+INSERT INTO products (product_name,department_name,price,stock_quantity)
+VALUES ("bowl", "accessories",59.99, 12);
