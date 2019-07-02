a6915c75e8a1c1aa1fd0664611c08c3176697159
Luzanne Burt <luzannea@gmail.com>
2019-07-02 09:56:08 -0400

    subtracting and updating

--- bamazon.js
+++ bamazon.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
-var Table = require('easy-table');
+var Table = require("easy-table");
+var inquirer = require ("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -16,17 +17,62 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-  });
-  
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.table(res);
-      console.log(Table.print(res));
-      connection.end();
-    });
-  }
-  
+  if (err) throw err;
+    start();   
+});
+
+function showTable(){
+  connection.query("SELECT * FROM products",
+  function(err, res) {
+  if (err) throw err;
+  // Log all results of the SELECT statement
+  console.table();
+  })};
+
+function start(res){
+  connection.query("SELECT * FROM products",
+  function(err, res) {
+  if (err) throw err;
+  // Log all results of the SELECT statement
+  console.table(res);
+
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "product",
+      message:"What would you like to buy?"
+    },
+    {
+      type:"input",
+      name:"qty",
+      message: "How many would you like to buy?"
+    }
+  ]).then(function(answer){
+      connection.query("UPDATE products SET ? WHERE ?",  
+       [{stock_quantity:res[0].stock_quantity - answer.qty},{product_name:answer.product}],
+       function(err, update) {
+        if (err) throw err;
+      showTable();  
+      exit();
+
+  // end query
+       })
+    
+  //end .then 
+  })
+// end query
+});
+// end start()
+}
+
+
+
+
+
+
+
+
+// if user selects exit
+function exit(){
+  connection.end();
+}
