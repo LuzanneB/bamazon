44d2e2128cfa9d913edc8abb156d81aad22510d9
Luzanne Burt <luzannea@gmail.com>
2019-07-03 20:40:00 -0400

    updateTable function added

--- bamazon.js
+++ bamazon.js
@@ -44,6 +44,7 @@ function start(){
       message: "How many would you like to buy?"
     }
   ]).then(function(answer){
+    // store the answers into variables to use in updateTable()
       let purchaseQty = answer.qty;
       let productChoice = answer.product;
       updateTable(purchaseQty,productChoice);    
@@ -52,11 +53,30 @@ function start(){
 // end start()
 }
 
-
-
-
-
-
+// update table and let customer know what their total is
+function updateTable(purchaseQty, productChoice){
+  // end function
+  let query= "SELECT * FROM products WHERE ?";
+  connection.query(query,[{product_name = productChoice}],
+     function(err,res){
+      if (err) throw err;
+      // check to see if we have enough in stock
+      if (purchaseQty <= res[0].stock_quantity){
+        let totalPurchase = res[0].price * purchaseQty;
+        // let the customer know we have it in stock and what total purchase is
+        console.log("Everything is in Stock!");
+        console.log("You're total purchase comes to " + totalPurchase + " for a quantity of " + purchaseQty + ", on item: " + productChoice + ".");
+        // update table 
+        connection.query("UPDATE products SET ? WHERE ?",[{stock_quantity: res[0].stock_quantity-purchaseQty},{product_name: productChoice}])
+      }
+      // let customer know we don't have enough in stock
+      else{
+        console.log("I'm sorry, we don't have enough "+ productChoice + " to complete your order.");
+      }
+      showTable();
+    });
+//end updateTable() 
+}
 
 
 // if user selects exit
