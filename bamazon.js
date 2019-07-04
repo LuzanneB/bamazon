3696ad51b78598128e435f2635e389bcdf243a7d
Luzanne Burt <luzannea@gmail.com>
2019-07-03 20:05:26 -0400

    creating seperate functions - change approach

--- bamazon.js
+++ bamazon.js
@@ -1,8 +1,8 @@
-var mysql = require("mysql");
-var Table = require("easy-table");
-var inquirer = require ("inquirer");
+const mysql = require("mysql");
+const Table = require("easy-table");
+const inquirer = require ("inquirer");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
@@ -18,24 +18,20 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-    start();   
+    showTable();   
 });
 
 function showTable(){
-  connection.query("SELECT * FROM products",
-  function(err, res) {
-  if (err) throw err;
-  // Log all results of the SELECT statement
-  console.table();
-  })};
-
-function start(res){
-  connection.query("SELECT * FROM products",
+  let query="SELECT * FROM products"
+  connection.query(query,
   function(err, res) {
   if (err) throw err;
   // Log all results of the SELECT statement
   console.table(res);
+  start();
+  })};
 
+function start(){
   inquirer.prompt([
     {
       type: "input",
@@ -48,20 +44,11 @@ function start(res){
       message: "How many would you like to buy?"
     }
   ]).then(function(answer){
-      connection.query("UPDATE products SET ? WHERE ?",  
-       [{stock_quantity:res[0].stock_quantity - answer.qty},{product_name:answer.product}],
-       function(err, update) {
-        if (err) throw err;
-      showTable();  
-      exit();
-
-  // end query
-       })
-    
+      let purchaseQty = answer.qty;
+      let productChoice = answer.product;
+      updateTable(purchaseQty,productChoice);    
   //end .then 
   })
-// end query
-});
 // end start()
 }
 
